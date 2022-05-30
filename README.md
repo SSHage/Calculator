880546f5e609c301d2590dc4cf627c63b8d19fde
Seah Siang Hwee <seahsianghwee1995@hotmail.com>
2022-05-30 21:57:38 +0800

    Improved functionality for clearing display, decimal can only be clicked once

--- README.md
+++ README.md
@@ -3,12 +3,17 @@
 Progress Chart:
 
 22/5/22:
-Add the basic design of the calculator.
+Created the basic design of the calculator.
 
 23/5/22
 Added functionality of buttons. Numbers clicked should be reflected on the display.
 
+30/5/22:
+Prevent numbers from being cleared on clicking any operators by using <getAttribute('id')> on the clear button
+Prevent decimal point from being added more than once.
+
 To do:
 > Add in functionality for operators. 
-> Prevent numbers from being cleared on clicking of any operator.
-> Find out how to disable 2 bottom right cells from being clicked. Is there a need to use button for them?
+
+Remarks:
+Additional functionality can be added for bottom two right buttons in the future.
--- main.js
+++ main.js
@@ -45,25 +45,56 @@ function operate(operator, num1, num2){
     }
 }
 
-//
-var display=document.querySelector("display");
+// function to add operators
+var equals=document.querySelector("#equals");
+equals.innerHTML="=";
+
+var addition=document.querySelector("#addition");
+addition.innerHTML="+";
+
+var subtraction=document.querySelector("#subtraction");
+subtraction.innerHTML="-";
+
+var multiplication=document.querySelector("#multiplication");
+multiplication.innerHTML="×";
+
+var division=document.querySelector("#division");
+division.innerHTML="÷";
+
+var division=document.querySelector("#percentage");
+division.innerHTML="%";
+
+var division=document.querySelector("#clear");
+division.innerHTML="C";
 
 //function to record buttons clicked
+var display = document.querySelector("display");
 const buttons = document.querySelectorAll("button");
+
+let decimalCount = 0; //variable to count decimals inputted.
 
 for (const button of buttons){
     button.addEventListener("click",function(e){
         this.classList.add("clicked");
-            // append to display if number button clicked
             if (this.classList.contains("number")){
-                display.innerHTML += this.innerHTML;
-                // shrink font if >9 digits
-                if (display.innerHTML.length > 9){
-                    display.classList.add("shrink");
+                if (this.classList.contains('decimal') && decimalCount == 0){
+                    display.innerHTML += this.innerHTML; // append to display if number button clicked
+                    decimalCount++;
+                    console.log(decimalCount);
+                    }
+                else if (this.classList.contains('decimal') && decimalCount == 1){
+                    }
+                else {
+                    display.innerHTML += this.innerHTML;
+                }
+                
+                if (display.innerHTML.length > 8){ 
+                    display.classList.add("shrink"); // shrink font if >9digits
                     }
                 }
             // clear display if C is pressed
-            else if (this.innerHTML.value = "C"){
+            else if (this.getAttribute('id') == "clear"){
+                console.log(this.innerHTML);
                 display.innerHTML = "";
                 display.classList.remove("shrink");
             }
