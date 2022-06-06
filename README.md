4685d31c53a0c4e68458ec07fbd577e439248f27
Seah Siang Hwee <seahsianghwee1995@hotmail.com>
2022-06-06 23:58:40 +0800

    Added limited functionality for operators

--- README.md
+++ README.md
@@ -5,18 +5,21 @@ Progress Chart:
 22/05/22:
 Created the basic design of the calculator.
 
-23/05/22
+23/05/22:
 Added functionality of buttons. Numbers clicked should be reflected on the display.
 
 30/05/22:
 Prevent numbers from being cleared on clicking any operators by using <getAttribute('id')> on the clear button
 Prevent decimal point from being added more than once.
 
-01/06/22
+01/06/22:
 Added some colours to the calculator
 
+06/06/22:
+Added functionality for operators
+
 To do:
-> Add in functionality for operators. 
+> Add in functionality for multiple digit operators. To solve logic for '0 values'.
 
 Remarks:
 Additional functionality can be added for bottom two right buttons in the future.
--- main.js
+++ main.js
@@ -31,17 +31,17 @@ function divide() {
 }
 
 function operate(operator, num1, num2){
-    if (operator == '+'){
-        add(num1, num2);
+    if (operator == 'addition'){
+        return add(num1, num2);
     }
-    else if (operator == '-'){
-        subtract(num1,num2);
+    else if (operator == 'subtraction'){
+        return subtract(num1, num2);
     }
-    else if (operator == '×'){
-        multiply(num1,num2);
+    else if (operator == 'multiplication'){
+        return multiply(num1, num2);
     }
-    else if (operator == '÷'){
-        divide(num1,num2);
+    else if (operator == 'division'){
+        return divide(num1, num2);
     }
 }
 
@@ -71,38 +71,61 @@ division.innerHTML="C";
 var display = document.querySelector("display");
 const buttons = document.querySelectorAll("button");
 
-let decimalCount = 0; //variable to count decimals inputted.
+let decimalCount = 0; // variable to count decimals inputted.
+let num1, num2=0; // variables to insert into operator
+let operator; // variable to store operator string
+let finalNum = 0;
 
 for (const button of buttons){
     button.addEventListener("click",function(e){
-        this.classList.add("clicked");
+        this.classList.add("clicked"); // add animation for clicked button
             if (this.classList.contains("number")){
                 if (this.classList.contains('decimal') && decimalCount == 0){
-                    display.innerHTML += this.innerHTML; // append to display if number button clicked
+                    display.innerHTML += this.innerHTML; // append decimal to display if decimal button clicked
                     decimalCount++;
-                    console.log(decimalCount);
                     }
-                else if (this.classList.contains('decimal') && decimalCount == 1){
+                else if (this.classList.contains('decimal') && decimalCount == 1){ // do not do anything if decimal clicked already
                     }
                 else {
-                    display.innerHTML += this.innerHTML;
+                    display.innerHTML += this.innerHTML; //append number to display
+                    num1 = parseFloat(display.innerHTML); //store value of number
+                    console.log("num1 is " + num1);
+                    console.log("num2 is " + num2);
                 }
                 
                 if (display.innerHTML.length > 8){ 
                     display.classList.add("shrink"); // shrink font if >9digits
                     }
                 }
-            // clear display if C is pressed
-            else if (this.getAttribute('id') == "clear"){
-                console.log(this.innerHTML);
+            else if (this.getAttribute('id') == "clear"){ // clear display if C is clicked
                 display.innerHTML = "";
-                display.classList.remove("shrink");
+                display.classList.remove("shrink"); // shrink if that are >9digits
+                num1 = 0; //reset values to 0
+                num2 = 0;
+                finalNum = 0;
             }
 
+            else if (this.classList.contains("operator")){
+                operator = this.id; // store operator as latest clicked operator button
+                display.innerHTML = "";
+                display.classList.remove("shrink"); // shrink if that are >9digits
+            }
+
+            if (num1 !=0 && num2 !=0) { // check if both numbers are not 0
+                finalNum = operate(operator,num2,num1); 
+                num2 = finalNum; 
+                num1 = 0;
+            }
+            else if (num1 !=0 && num2 == 0) {
+                num2 = num1; // if num1 already has a number, store num2 and reset num1 to 0
+                num1 = 0;
+                //console.log(this.innerHTML);
+            }
             
+            console.log("finalNum is " + finalNum);
         })
     button.addEventListener("transitionend",function(e){
-        this.classList.remove("clicked");
+        this.classList.remove("clicked"); // remove animation for clicked button
     })   
 }
 
